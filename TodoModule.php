4ee5d8b316279d2c27c5a536b7fd4033be94db01
sabian <olegsabian@gmail.com>
2015-09-10 22:34:27 +0300

    Основные файлы модуля

---
+++ TodoModule.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * ToDo module
+ *
+ * @package  yupe.modules.payler
+ * @author   Oleg Filimonov <olegsabian@gmail.com>
+ * @license  BSD http://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_BSD
+ * @version  0.1
+ * @link     https://github.com/sabian/yupe-todo
+ **/
+
+use yupe\components\WebModule;
+
+class TodoModule extends WebModule
+{
+    public function getDependencies()
+    {
+        return [];
+    }
+
+    public function getVersion()
+    {
+        return '0.1';
+    }
+
+    public function getCategory()
+    {
+        return 'Контент';
+    }
+
+    public function getName()
+    {
+        return 'ToDo';
+    }
+
+    public function getDescription()
+    {
+        return 'Простой модуль для управления задачами';
+    }
+
+    public function getAuthor()
+    {
+        return 'Команда Yupe';
+    }
+
+    public function getAuthorEmail()
+    {
+        return 'team@yupe.ru';
+    }
+
+    public function getUrl()
+    {
+        return 'http://yupe.ru';
+    }
+
+    public function getIcon()
+    {
+        return 'fa fa-fw fa-thumb-tack';
+    }
+
+    public function init()
+    {
+        parent::init();
+
+        $this->setImport(
+            [
+                'application.modules.todo.models.*',
+                'application.modules.todo.components.widgets.*',
+            ]
+        );
+    }
+
+    public function getAdminPageLink()
+    {
+        return '/todo/todoBackend/index';
+    }
+
+    public function getNavigation()
+    {
+        return [
+            [
+                'icon'  => 'fa fa-fw fa-list-alt',
+                'label' => 'Список задач',
+                'url'   => ['/todo/todoBackend/index']
+            ],
+            [
+                'icon'  => 'fa fa-fw fa-plus-square',
+                'label' => 'Создать задачу',
+                'url'   => ['/todo/todoBackend/create']
+            ],
+        ];
+    }
+}
---
+++ install/todo.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'module'    => [
+        'class' => 'application.modules.todo.TodoModule',
+    ],
+];
